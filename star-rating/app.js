72f7610e43d004f9cb9eb81e3843e8fe7287c094
Nisham P <nishamp@Nishams-MacBook-Pro.local>
2024-03-05 20:13:01 +0530

    add mouse over and mouse leave functionality

--- star-rating/app.js
+++ star-rating/app.js
@@ -1,6 +1,6 @@
 var allStars = document.querySelectorAll('.star')
 console.log(allStars)
-
+let pidx = 0;
 // for(let i = 0 ; i < allStars.length; i++) {
 //     allStars[i].addEventListener('click', () => {
 //         console.log(' clicked' , i+1)
@@ -13,25 +13,38 @@ starContainer.addEventListener('click', (e) => {
 
     var element = e.target
     var required = element.hasAttribute('idx')
-    console.log(required)
-    if(!required) return
-    var idx = element.getAttribute('idx')
-    fillStars(idx)
-    
+    if (!required) return
+    pidx = element.getAttribute('idx')
+    fillStars(pidx)
+
 
 })
+
+starContainer.addEventListener('mouseover', (e) => {
+    var element = e.target
+    var required = element.hasAttribute('idx')
+    if (!required) return
+    var idx = element.getAttribute('idx')
+    fillStars(idx)
+})
+
+
+starContainer.addEventListener('mouseleave', () => {
+    fillStars(pidx)
+})
+
 
 
 const fillStars = (idx) => {
     const countSpan = document.getElementById('count')
     countSpan.textContent = idx
-    
+
     console.log(allStars.length)
-    for(let i = 0; i< allStars.length ; i++) {
+    for (let i = 0; i < allStars.length; i++) {
         allStars[i].classList.remove('yellow')
     }
 
-    for(let i = 0; i< idx ; i++) {
+    for (let i = 0; i < idx; i++) {
         allStars[i].classList.add('yellow')
     }
 }
